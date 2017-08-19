de4864dc0697c7dd877d16c05554325c1ba5466d
fmd123 <fmdavis6@gmail.com>
2017-08-18 21:05:12 -0600

    add mouseup/mousedown functionality

--- pixelArt.js
+++ pixelArt.js
@@ -1,14 +1,10 @@
 let colors = ["gold", "deeppink","darkorange", "cadetblue", "mediumorchid", "mediumseagreen", "limegreen", "tomato", "skyblue", "slategrey", "yellow", "darkred", "dodgerblue", "crimson", "lightpink", "khaki"];
 let brushColor;
-// someColors(colors){
-//   for(let l=0; l<3; l++){
-//     for(let c = 0; c<colors.length; c++){
-//       sampleColor[i] = color[Math.floor(Math.random()*3 )]
-//     }
-//     console.log(sampleColor[i]);
-//   }
-$(document).ready(function(){
 
+https://stackoverflow.com/questions/1104344/what-is-the-difference-between-the-mouseover-and-mouseenter-events
+
+$(document).ready(function(){
+var pressed = false;
   // for (let k =0; k<3; k++){
   //   $(".colorSample").css("background-color", brushColor
   //   });
@@ -21,24 +17,51 @@ $(document).ready(function(){
           $(".row").append("<div class = 'pixel'><div>")
         }
     }
-    $(".pixel").on("mouseenter", function(){
-        $(event.target ).css( "background-color", brushColor);
+    $(".pixel").on("mousedown", function(){
+      pressed = true;
+      $(event.target ).css( "background-color", brushColor);
     });
 
+
+    $(".pixel").on("mouseenter", function(){
+      if(pressed === true){
+        $(event.target ).css( "background-color", brushColor);
+      }
+    });
+
+    $(".pixel").on("mouseup", function(){
+      pressed = false;
+      //$(event.target ).css( "background-color", null);
+    });
+
+
     $("#brush1").on("click", function(){
-      brushColor = "chartreuse";
+      brushColor = "limegreen";
+      $(event.target).css("border", "3px solid blue");
     });
 
     $("#brush2").on("click", function(){
       brushColor = "red";
+      $(event.target).css("border", "3px solid blue");
     });
 
     $("#brush3").on("click", function(){
       brushColor = "cornflowerblue";
+      $(event.target).css("border", "3px solid blue");
     });
+
     $("#brush4").on("click", function(){
       brushColor = "white";
+      $(event.target).css("border", "3px solid blue");
     });
 
 
 });
+
+
+    // randomColors(arr){
+    //
+    // // for(let c = 0; c<colors.length; c++){
+    // brushColor = colors[Math.floor(Math.random()*3 )]
+    // console.log(brushColor);
+    // }
