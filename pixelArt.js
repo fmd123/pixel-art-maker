e6c6a15d28970b6c4b46b927e48d72fe32396448
fmd123 <fmdavis6@gmail.com>
2017-08-18 18:39:12 -0600

    has a grid

---
+++ pixelArt.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+  for(let i=0; i<10; i++){
+      $(".pixelHolder").append("<div class = 'row'><div>");
+        for(j=0; j<10; j++){
+          $(".row").append("<div class = 'pixel'><div>")
+        }
+    }
+    // $(".pixel").on("click", function(){
+    //   $(".pixel").
+    // })
+
+
+});
